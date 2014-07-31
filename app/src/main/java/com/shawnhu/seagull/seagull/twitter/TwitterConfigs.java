67d35633ed4bb4c58329f531b0ef2dfc5ed348d9
HuaiShun Hu <hshu@ambarella.com>
2014-07-31 16:19:37 +0800

    add twitter4j

---
+++ app/src/main/java/com/shawnhu/seagull/seagull/twitter/TwitterConfigs.java
@@ -0,0 +1,9 @@
+package com.shawnhu.seagull.seagull.twitter;
+
+/**
+ * Created by shawn on 14-7-31.
+ */
+public class TwitterConfigs {
+
+    public static final String OAUTH_CALLBACK_OOB = "oob";
+}
