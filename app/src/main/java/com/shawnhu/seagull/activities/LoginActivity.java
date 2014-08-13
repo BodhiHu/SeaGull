6e30d7f196c5fe8394b10f4df776b2641df036e8
HuaiShun Hu <hshu@ambarella.com>
2014-08-13 20:04:00 +0800

    sync

--- app/src/main/java/com/shawnhu/seagull/activities/LoginActivity.java
+++ app/src/main/java/com/shawnhu/seagull/activities/LoginActivity.java
@@ -29,29 +29,20 @@ import java.util.ArrayList;
 import java.util.List;
 import com.shawnhu.seagull.R;
 
-/**
- * A login screen that offers login via email/password.
-
- */
 public class LoginActivity extends Activity implements LoaderCallbacks<Cursor>{
 
-    /**
-     * A dummy authentication store containing known user names and passwords.
-     * TODO: remove after connecting to a real authentication system.
-     */
-    private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "foo@example.com:hello", "bar@example.com:world"
+    private static final String[]   DUMMY_CREDENTIALS = new String[]{
+            "foo@example.com:hello",
+            "bar@example.com:world",
     };
-    /**
-     * Keep track of the login task to ensure we can cancel it if requested.
-     */
-    private UserLoginTask mAuthTask = null;
+
+    private UserLoginTask           mAuthTask = null;
 
     // UI references.
-    private AutoCompleteTextView mEmailView;
-    private EditText mPasswordView;
-    private View mProgressView;
-    private View mLoginFormView;
+    private AutoCompleteTextView    mEmailView;
+    private EditText                mPasswordView;
+    private View                    mProgressView;
+    private View                    mLoginFormView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -96,12 +87,6 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor>{
         }
     }
 
-
-    /**
-     * Attempts to sign in or register the account specified by the login form.
-     * If there are form errors (invalid email, missing fields, etc.), the
-     * errors are presented and no actual login attempt is made.
-     */
     public void attemptLogin() {
         if (mAuthTask != null) {
             return;
@@ -198,15 +183,12 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor>{
     @Override
     public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
         return new CursorLoader(this,
-                // Retrieve data rows for the device user's 'profile' contact.
                 Uri.withAppendedPath(ContactsContract.Profile.CONTENT_URI,
-                        ContactsContract.Contacts.Data.CONTENT_DIRECTORY), ProfileQuery.PROJECTION,
-
-                // Select only email addresses.
-                ContactsContract.Contacts.Data.MIMETYPE +
-                        " = ?", new String[]{ContactsContract.CommonDataKinds.Email
-                                                                     .CONTENT_ITEM_TYPE},
-
+                        ContactsContract.Contacts.Data.CONTENT_DIRECTORY),
+                ProfileQuery.PROJECTION,
+                // Select email addresses.
+                ContactsContract.Contacts.Data.MIMETYPE + " = ?",
+                    new String[]{ContactsContract.CommonDataKinds.Email.CONTENT_ITEM_TYPE},
                 // Show primary email addresses first. Note that there won't be
                 // a primary email address if the user hasn't specified one.
                 ContactsContract.Contacts.Data.IS_PRIMARY + " DESC");
@@ -278,10 +260,6 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor>{
         mEmailView.setAdapter(adapter);
     }
 
-    /**
-     * Represents an asynchronous login/registration task used to authenticate
-     * the user.
-     */
     public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
 
         private final String mEmail;
