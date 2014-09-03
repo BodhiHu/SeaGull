08484f700cab9966210f92e40acc804310108711
Shawn Hu <huhuaishun@gmail.com>
2014-09-04 03:40:35 +0800

    smooth drag down

--- app/src/main/java/com/shawnhu/seagull/utils/AnimationUtils.java
+++ app/src/main/java/com/shawnhu/seagull/utils/AnimationUtils.java
@@ -25,68 +25,59 @@ public class AnimationUtils {
                                      final float fromAlpha,   final float toAlpha,
                                      final long  duration,
                                      ValueAnimator.AnimatorUpdateListener l) {
+        //scroll
         ObjectAnimator xTranslate = ObjectAnimator.ofInt(scrollView, "scrollX", fromScrollX, toScrollX);
         ObjectAnimator yTranslate = ObjectAnimator.ofInt(scrollView, "scrollY", fromScrollY, toScrollY);
+
+        //resize height
+        ValueAnimator heightAnimator =
+                ValueAnimator.ofInt(scrollView.getHeight(), scrollView.getHeight() + (toScrollY - fromScrollY));
+        heightAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                Integer value = (Integer) animation.getAnimatedValue();
+
+                ViewGroup.LayoutParams layoutParams = scrollView.getLayoutParams();
+                layoutParams.height = value;
+                scrollView.setLayoutParams(layoutParams);
+            }
+        });
+
+        //resize width
+        ValueAnimator widthAnimator =
+                ValueAnimator.ofInt(scrollView.getWidth(), scrollView.getWidth() + (toScrollX - fromScrollX));
+        widthAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                Integer value = (Integer) animation.getAnimatedValue();
+
+                ViewGroup.LayoutParams layoutParams = scrollView.getLayoutParams();
+                layoutParams.width = value;
+                scrollView.setLayoutParams(layoutParams);
+            }
+        });
+
         AnimatorSet animators = new AnimatorSet();
         animators.setDuration(duration);
-        animators.playTogether(xTranslate, yTranslate);
+        animators.playTogether(xTranslate, yTranslate, heightAnimator);
         animators.setInterpolator(new AccelerateDecelerateInterpolator());
 
         ValueAnimator.AnimatorUpdateListener alphaAnimatorListner =
-            new ValueAnimator.AnimatorUpdateListener() {
-                @Override public void onAnimationUpdate(ValueAnimator animation) {
-                    long currentTimeMillis = animation.getCurrentPlayTime();
-                    float fraction = ((float) (currentTimeMillis) / duration);
-                    fraction = fraction <= 1 ? fraction : 1;
+                new ValueAnimator.AnimatorUpdateListener() {
+                    @Override
+                    public void onAnimationUpdate(ValueAnimator animation) {
+                        long currentTimeMillis = animation.getCurrentPlayTime();
+                        float fraction = ((float) (currentTimeMillis) / duration);
+                        fraction = fraction <= 1 ? fraction : 1;
 
-                    float alpha = fromAlpha + (toAlpha - fromAlpha) * fraction;
-                    Animation alphaAnima = new AlphaAnimation(alpha, alpha);
-                    alphaAnima.setDuration(0);
-                    alphaAnima.setFillAfter(true);
+                        float alpha = fromAlpha + (toAlpha - fromAlpha) * fraction;
+                        Animation alphaAnima = new AlphaAnimation(alpha, alpha);
+                        alphaAnima.setDuration(0);
+                        alphaAnima.setFillAfter(true);
 
-                    alphaView.startAnimation(alphaAnima);
-                }
-            };
-        ValueAnimator.AnimatorUpdateListener layoutAnimatorListener =
-            new ValueAnimator.AnimatorUpdateListener() {
-                int startScrollX = scrollView.getScrollX();
-                int startScrollY = scrollView.getScrollY();
-                int startHeight  = scrollView.getHeight();
-                int startWidth   = scrollView.getWidth();
-
-                @Override public void onAnimationUpdate(ValueAnimator animation) {
-                    int nextHeight = startHeight + (scrollView.getScrollY() - startScrollY);
-                    int nextWidth = startWidth + (scrollView.getScrollX() - startScrollX);
-                    ViewGroup.LayoutParams layout = scrollView.getLayoutParams();
-                    layout.height = nextHeight;
-                    layout.width = nextWidth;
-                    scrollView.setLayoutParams(layout);
-                }
-            };
-        Animator.AnimatorListener endLayoutFixListener = new Animator.AnimatorListener() {
-            int startScrollX;
-            int startScrollY;
-            int startHeight;
-            int startWidth;
-            @Override public void onAnimationStart(Animator animation) {
-                startScrollX = scrollView.getScrollX();
-                startScrollY = scrollView.getScrollY();
-                startHeight  = scrollView.getHeight();
-                startWidth   = scrollView.getWidth();
-            }
-            @Override public void onAnimationEnd(Animator animation) {
-                int endScrollX = scrollView.getScrollX();
-                int endScrollY = scrollView.getScrollY();
-
-                ViewGroup.LayoutParams layoutParams = scrollView.getLayoutParams();
-                layoutParams.height = startHeight + (endScrollY - startScrollY);
-                layoutParams.width  = startWidth  + (endScrollX - startScrollX);
-
-                scrollView.setLayoutParams(layoutParams);
-            }
-            @Override public void onAnimationCancel(Animator animation) {  }
-            @Override public void onAnimationRepeat(Animator animation) {  }
-        };
+                        alphaView.startAnimation(alphaAnima);
+                    }
+                };
 
         for (Animator animator : animators.getChildAnimations()) {
             ValueAnimator valueAnimator = (ValueAnimator) animator;
@@ -94,15 +85,12 @@ public class AnimationUtils {
                 //alpha
                 valueAnimator.addUpdateListener(alphaAnimatorListner);
             }
-            //this will have visible delay
-            valueAnimator.addUpdateListener(layoutAnimatorListener);
             if (l != null) {
                 //custom
                 valueAnimator.addUpdateListener(l);
             }
         }
 
-        animators.addListener(endLayoutFixListener);
         animators.start();
     }
 
